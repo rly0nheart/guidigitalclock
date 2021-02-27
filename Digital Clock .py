458ffb9e3ccc927ab0f2e3a96073870aece6c0a7
eihctir <74001397+rlyonheart@users.noreply.github.com>
2021-02-28 00:47:02 +0200

    Add files via upload

---
+++ Digital Clock .py
@@ -0,0 +1,26 @@
+from tkinter import *
+import time
+
+'''time function'''
+def times():
+    
+    current_time = time.strftime ("%I:%M:%S:%p")
+    clock_lbl = Label(root , font="Times 11", bg="black",fg="red",text=current_time )
+    clock_lbl.after(200, times)
+    clock_lbl.place(x=60, y=60)
+
+'''main gui window'''    
+root=Tk()
+
+root.title("Digital Clock")
+root.geometry("500x300")
+root.resizable(False,False)
+root.configure(background="black")
+
+'''quit button widget'''
+quitButton = Button(root, text="Close",font="Times 4",fg="red",bg="black",command=root.quit)
+quitButton.place(x=170,y=190)
+times()
+    
+root.mainloop()
+
