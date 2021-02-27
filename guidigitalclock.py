1a3047996c52894034398016b1fe67143988d9e9
eihctir <74001397+rlyonheart@users.noreply.github.com>
2021-02-28 00:53:40 +0200

    Update and rename Digital Clock .py to guidigitalclock.py

--- guidigitalclock.py
+++ guidigitalclock.py
@@ -1,7 +1,10 @@
+'''Simple Tkinter GUI Digital Clock''' 
+
+# importing required modules
 from tkinter import *
 import time
 
-'''time function'''
+# time function
 def times():
     
     current_time = time.strftime ("%I:%M:%S:%p")
@@ -9,7 +12,7 @@ def times():
     clock_lbl.after(200, times)
     clock_lbl.place(x=60, y=60)
 
-'''main gui window'''    
+# main gui window
 root=Tk()
 
 root.title("Digital Clock")
@@ -17,10 +20,17 @@ root.geometry("500x300")
 root.resizable(False,False)
 root.configure(background="black")
 
-'''quit button widget'''
+# quit button widget
 quitButton = Button(root, text="Close",font="Times 4",fg="red",bg="black",command=root.quit)
 quitButton.place(x=170,y=190)
 times()
     
 root.mainloop()
 
+'''Author Info''' 
+'''
+   Name: Eihctir A.K.A Rlyonheart
+   Github| www.github.com/rlyonheart
+   Twitter| www.twitter.com/rly0nheart
+   Instagram| www.instagram.com/rlyonheart
+'''
